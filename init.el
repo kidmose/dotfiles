97db78c00301f6555c260b72aedcf76b07800efe
Egon Kidmose <kidmose@gmail.com>
2013-05-08 20:41:08 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,19 @@
+
+
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
+
+(global-unset-key "\C-z")
