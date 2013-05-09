58f46be8c4fee089d54c1a55e2dc9dbeb409d0a8
Egon Kidmose <ekidmo09@student.aau.dk>
2013-05-09 10:40:46 +0200

    merged in config from laptop, needs clean up

--- init.el
+++ init.el
@@ -15,10 +15,69 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
+;; Clipboard to and from other programs
+(setq x-select-enable-clipboard t)
+(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+;; AucTex
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(setq TeX-auto-save t) ; parse when saving
+(setq TeX-parse-self t); parse when loading
+(setq TeX-PDF-mode t); pdf by default
+
+;; Evince as pdf-viewer with two way search using dbus
+;http://www.emacswiki.org/emacs/AUCTeX#toc20
+(require 'dbus)
+
+(defun un-urlify (fname-or-url)
+  "A trivial function that replaces a prefix of file:/// with just /."
+  (if (string= (substring fname-or-url 0 8) "file:///")
+      (substring fname-or-url 7)
+    fname-or-url))
+
+(defun th-evince-sync (file linecol &rest ignored)
+  (let* ((fname (un-urlify file))
+         (buf (find-buffer-visiting fname))
+         (line (car linecol))
+         (col (cadr linecol)))
+    (if (null buf)
+        (message "[Synctex]: %s is not opened..." fname)
+      (switch-to-buffer buf)
+      (goto-line (car linecol))
+      (unless (= col -1)
+        (move-to-column col)))))
+
+(defvar *dbus-evince-signal* nil)
+
+(defun enable-evince-sync ()
+  (require 'dbus)
+  (when (and
+         (eq window-system 'x)
+         (fboundp 'dbus-register-signal))
+    (unless *dbus-evince-signal*
+      (setf *dbus-evince-signal*
+            (dbus-register-signal
+             :session nil "/org/gnome/evince/Window/0"
+             "org.gnome.evince.Window" "SyncSource"
+             'th-evince-sync)))))
+
+(add-hook 'LaTeX-mode-hook 'enable-evince-sync)
+
+;(setq-default TeX-master nil); ask for master when opening tex files
 (put 'upcase-region 'disabled nil)
 
-;;;; Global appearance
-(setq default-tab-width 2)
+;; Avoid accicential sleepingz
+(global-unset-key (kbd "C-z"))
 
-;;;; Global key bindings
-(global-unset-key "\C-z")
+;;;; Unbind arrow movement, for training myself not to use proper keys
+(global-unset-key [left])
+(global-unset-key [up])
+(global-unset-key [right])
+(global-unset-key [down])
+
+;; Use tab for indentation in c and cpp (CC major mode) when saving
+ (setq-default c-basic-offset 2
+                  tab-width 2
+                  indent-tabs-mode t)
