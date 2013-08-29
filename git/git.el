ea259f41ff0835a85b9be6324ecbf273e1fdd9f6
Egon Kidmose <kidmose@gmail.com>
2013-08-28 22:46:35 -0700

    see prev.

---
+++ git/git.el
@@ -0,0 +1,8 @@
+;; -----------------------------------------------------------------------------
+;; Git support in vc-mode
+;; -----------------------------------------------------------------------------
+; run "sudo apt-get install git-el" for installing the below files on debian (So they are not needed)
+;(load "/usr/share/git-core/emacs/git.el")
+;(load "/usr/share/git-core/emacs/git-blame.el")
+;(load "/usr/share/git-core/emacs/vc-git.el")
+;(add-to-list 'vc-handled-backends 'GIT)
---
+++ latex/latex.el
@@ -0,0 +1,66 @@
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+;; Clipboard to and from other programs
+(setq x-select-enable-clipboard t)
+(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+;; AucTex
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(setq TeX-auto-save t) ; parse when saving
+(setq TeX-parse-self t); parse when loading
+(setq TeX-PDF-mode t); pdf by default
+
+;; Evince as pdf-viewer with two way search using dbus
+;http://www.emacswiki.org/emacs/AUCTeX#toc20
+(require 'dbus)
+
+(defun un-urlify (fname-or-url)
+  "A trivial function that replaces a prefix of file:/// with just /."
+  (if (string= (substring fname-or-url 0 8) "file:///")
+      (substring fname-or-url 7)
+    fname-or-url))
+
+(defun th-evince-sync (file linecol &rest ignored)
+  (let* ((fname (un-urlify file))
+         (buf (find-buffer-visiting fname))
+         (line (car linecol))
+         (col (cadr linecol)))
+    (if (null buf)
+        (message "[Synctex]: %s is not opened..." fname)
+      (switch-to-buffer buf)
+      (goto-line (car linecol))
+      (unless (= col -1)
+        (move-to-column col)))))
+
+(defvar *dbus-evince-signal* nil)
+
+(defun enable-evince-sync ()
+  (require 'dbus)
+  (when (and
+         (eq window-system 'x)
+         (fboundp 'dbus-register-signal))
+    (unless *dbus-evince-signal*
+      (setf *dbus-evince-signal*
+            (dbus-register-signal
+             :session nil "/org/gnome/evince/Window/0"
+             "org.gnome.evince.Window" "SyncSource"
+             'th-evince-sync)))))
+
+(add-hook 'LaTeX-mode-hook 'enable-evince-sync)
+
+;(setq-default TeX-master nil); ask for master when opening tex files
+(put 'upcase-region 'disabled nil)
