36a1522636ffea59643d3f1c34e42be5d599ff30
Egon Kidmose <kidmose@gmail.com>
2014-05-14 09:04:17 +0200

    working latex

--- latex/latex.el
+++ latex/latex.el
@@ -66,3 +66,16 @@
 
 ;; ;(setq-default TeX-master nil); ask for master when opening tex files
 ;; (put 'upcase-region 'disabled nil)
+
+
+;;; Okular as pdfviewer with fwd and rev search
+;; (custom-set-variables
+;;   ;; custom-set-variables was added by Custom.
+;;   ;; If you edit it by hand, you could mess it up, so be careful.
+;;   ;; Your init file should contain only one such instance.
+;;   ;; If there is more than one, they won't work right.
+;;  '(TeX-source-correlate-method (quote synctex))
+;;  '(TeX-source-correlate-mode t)
+;;  '(TeX-source-correlate-start-server t)
+;;  '(TeX-view-program-list (quote (("Okular" "okular –unique %o#src:%n%b"))))
+;;  '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "xpdf") (output-html "xdg-open") (output-pdf "Okular")))))
