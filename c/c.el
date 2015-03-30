9b529a168da052ddbcb33583fac6f0ce40fc07a4
Egon Kidmose <kidmose@gmail.com>
2015-03-30 11:04:40 +0200

    cleaned up old init

--- c/c.el
+++
@@ -1,7 +0,0 @@
-;; Use tab for indentation in c and cpp (CC major mode) when saving
- (setq-default c-basic-offset 2
-                  tab-width 2
-                  indent-tabs-mode t)
-
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
--- term/term.el
+++
@@ -1,4 +0,0 @@
-;;;; term major mode
-; familiar, but basic yank on "C-y" (no kill ring)
-(add-hook 'term-mode-hook (lambda ()
-                            (define-key term-raw-map (kbd "C-y") 'term-paste)))
