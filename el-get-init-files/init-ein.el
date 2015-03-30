aaa661a783ee9b2ec9b39c1b348810d2eb49b668
Egon Kidmose <kidmose@gmail.com>
2015-03-30 14:28:54 +0200

    * el-get-init-files/init-ein.el: insert breakpoint

--- el-get-init-files/init-ein.el
+++ el-get-init-files/init-ein.el
@@ -1,3 +1,13 @@
+(defun ipython-breakpoint-add()
+  "Add a break point"
+  (interactive)
+  (beginning-of-line)
+  (indent-according-to-mode)
+  (insert "import ipdb; ipdb.set_trace()")
+  (newline)
+  (highlight-lines-matching-regexp "^[ ]*import ipdb; ipdb.set_trace()"))
+
 (add-hook 'ein:notebook-multilang-mode-hook
 	  (lambda () (local-set-key (kbd "C-c C-d") 'ein:worksheet-delete-cell)))
-
+(add-hook 'ein:notebook-multilang-mode-hook
+	  (lambda () (local-set-key (kbd "C-c b") 'ipython-breakpoint-add)))
