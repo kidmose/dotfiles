4de0eeef3e99b96a29a384a51d0238f7a96fc859
Egon Kidmose <kidmose@gmail.com>
2015-03-30 09:47:45 +0200

    omitting modules

--- glob_cust/glob_cust.el
+++ glob_cust/glob_cust.el
@@ -39,8 +39,7 @@ delete-by-moving-to-trash t
 
 ;;;; Modes with only little customisation
 ;; Markdown
-(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
+;; (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
+;; (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+;; (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+;; (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
--- init.el
+++ init.el
@@ -3,15 +3,15 @@
 
 ;; Load seperate files
 ; Git
-(load "~/.emacs.d/git/git.el" nil t t)
+;(load "~/.emacs.d/git/git.el" nil t t)
 ; LaTeX
-(load "~/.emacs.d/latex/latex.el" nil t t)
+;(load "~/.emacs.d/latex/latex.el" nil t t)
 ; C and cpp
-(load "~/.emacs.d/c/c.el" nil t t)
+;(load "~/.emacs.d/c/c.el" nil t t)
 ; Global customisation
 (load "~/.emacs.d/glob_cust/glob_cust.el" nil t t)
 ; term
-(load "~/.emacs.d/term/term.el" nil t t)
+;(load "~/.emacs.d/term/term.el" nil t t)
 
 ;el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
