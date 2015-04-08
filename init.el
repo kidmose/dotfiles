4062e41a14fcd9158e69a5039568d05cb7e9beff
Egon Kidmose <kidmose@gmail.com>
2015-04-08 09:56:56 +0200

    * init.el: remove old, commented out term related

--- init.el
+++ init.el
@@ -10,22 +10,20 @@
 ;(load "~/.emacs.d/c/c.el" nil t t)
 ; Global customisation
 (load "~/.emacs.d/glob_cust/glob_cust.el" nil t t)
-; term
-;(load "~/.emacs.d/term/term.el" nil t t)
 
-;; el-get
+;;;; el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
+;; Downloads el-get:
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
-
+;; Where to store recipes (What are they?..)
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
-
+;; My own init files pr. mode:
 (setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
 
 ;; El get packages
