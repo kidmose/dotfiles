f0d59deeb12fc5f187e77dcfe1828123a78b9a8c
Egon Kidmose <kidmose@gmail.com>
2015-06-24 13:30:23 +0200

    from glob_cust to el-get-init-markdown

--- glob_cust/glob_cust.el
+++ glob_cust/glob_cust.el
@@ -35,11 +35,3 @@ delete-by-moving-to-trash t
 ;;;; Visual 
 ;; Load dark theme (emacs24)
 (load-theme 'tango-dark)
-
-
-;;;; Modes with only little customisation
-;; Markdown
-;; (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
-;; (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-;; (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-;; (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
