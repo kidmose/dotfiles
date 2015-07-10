827bc33b348d980bd54451acd75f456003d783b6
Egon Kidmose <kidmose@gmail.com>
2015-07-10 10:07:53 +0200

    AUCTeX: Fix missing parsing so BIBTeX is triggered
    http://www.mail-archive.com/auctex@gnu.org/msg05920.html

--- el-get-init-files/init-auctex.el
+++ el-get-init-files/init-auctex.el
@@ -16,8 +16,9 @@
 ;;;; AucTex
 ;;(load "auctex.el" nil t t)
 ;; (load "preview-latex.el" nil t t)
-;; (setq TeX-auto-save t) ; parse when saving
-;; (setq TeX-parse-self t); parse when loading
+(setq TeX-auto-save t) ; parse when saving
+(setq TeX-parse-self t); parse when loading
+
 (setq TeX-PDF-mode t); pdf by default
 (setq-default TeX-master nil) ; Query for master file.
 
