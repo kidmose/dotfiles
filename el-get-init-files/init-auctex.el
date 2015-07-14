8732cf4c6a0810499a52f3342b05548a44baa47b
Egon Kidmose <kidmose@gmail.com>
2015-07-14 08:56:45 +0200

    AUCTeX: Working correlate, both ways.
    Be ware: Moving to multiple `custom-set-variables`, should be ok as long
    as only edited by hand.

--- el-get-init-files/init-auctex.el
+++ el-get-init-files/init-auctex.el
@@ -16,6 +16,14 @@
 ;                             (setq-local comment-padding " ")))
 (setq reftex-plug-into-auctex t)
 
+; Synctex
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-mode t)
+)
 
 ; Evince
 ;; ;; Evince as pdf-viewer with two way search using dbus
