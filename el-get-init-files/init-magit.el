75a02f88f0130fe15fa50663db1ef73babe44041
dkEgoKid <egon.kidmose@lego.com>
2016-08-25 11:34:36 +0200

    clean up customize groups

--- el-get-init-files/init-magit.el
+++ el-get-init-files/init-magit.el
@@ -1,3 +1,4 @@
 (require 'magit-gitflow)
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 (setq magit-last-seen-setup-instructions "1.4.0")
+(setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
