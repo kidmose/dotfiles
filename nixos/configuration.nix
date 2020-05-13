3e4f3014c014719cfa2c9fa045da3f588637f347
Egon Kidmose <egk@es.aau.dk>
2020-05-13 15:09:18 +0200

    ssh-agent is running (Unsure if it integrates with kdewallet)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -90,6 +90,16 @@
 
   # Use kwallet to store sshkey and gpg passphrase (for instance for git)
   security.pam.services.kwallet.enableKwallet = true; # unlock on login
+  # It might be necessary to manually create a wallet with the default
+  # name (kdewallet) using a blowfish password, as the default of
+  # using a gpg password seems to break nextcloud integration.
+  #
+  # SSH
+  # start the agent, but identity needs to be added manually for each session?
+  programs.ssh = {
+    startAgent = true;
+    askPassword = "${pkgs.ksshaskpass}/bin/ksshaskpass";
+  };
   # GPG:
   programs.gnupg.agent.enable = true;
   # TODO: Assert that ~/.gnupg/gpg-agent.conf holds: "pinentry-program
