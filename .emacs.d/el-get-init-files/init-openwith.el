1d48dc79d5c723da7769523d4430ce6056d5c6a1
Egon Kidmose <egk@es.aau.dk>
2020-06-09 10:32:52 +0200

    [emacs] openwith doesn't prompt

--- .emacs.d/el-get-init-files/init-openwith.el
+++ .emacs.d/el-get-init-files/init-openwith.el
@@ -2,7 +2,34 @@
 (setq
  openwith-associations
  (quote(
+	;; ("\\.mp4\\'" "evince" (file))
+	;; ("\\.mkv\\'" "eog" (file))
 	("\\.pdf\\'" "evince" (file))
-	("\\.\\(?:jp?g\\|png\\)\\'" "eog" (file))
+	("\\.jpg\\'" "eog" (file))
+	("\\.jpeg\\'" "eog" (file))
+	("\\.png\\'" "eog" (file))
 	("\\.odt\\'" "libreoffice" (file))
+	("\\.docx\\'" "libreoffice" (file))
 	)))
+
+;; Do not nag when openning large files
+;; Inspired by https://emacs.stackexchange.com/a/17096/8658
+(defvar my-ok-large-file-types
+  (concat
+   "\\.\\("
+   (mapconcat
+    #'(lambda (arg) (string-trim arg (rx "\\.") (rx "\\'")))
+    (mapcar 'car openwith-associations)
+    "\\|"
+    )
+   "\\)\\'"
+   )
+  ;; (rx "." (or "mp4" "mkv") string-end)
+  "Regexp matching filenames which are definitely ok to visit,
+even when the file is larger than `large-file-warning-threshold'.")
+
+(defadvice abort-if-file-too-large (around my-check-ok-large-file-types)
+  "If FILENAME matches `my-ok-large-file-types', do not abort."
+  (unless (string-match-p my-ok-large-file-types (ad-get-arg 2))
+    ad-do-it))
+(ad-activate 'abort-if-file-too-large)
