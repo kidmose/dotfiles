7f1a680e1c2a071381047a51e23e5d314af950df
Egon Kidmose <kidmose@gmail.com>
2020-10-19 16:38:04 +0200

    [emacs] use-package (remove el-get) and some basic org stuff

--- .emacs.d/el-get-init-files/init-magit.el
+++
@@ -1,7 +0,0 @@
-(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
-
-(setq magit-last-seen-setup-instructions "1.4.0")
-(setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
-(setq magit-diff-refine-hunk (quote all))
-
-(add-hook 'magit-mode-hook (lambda () (toggle-truncate-lines -1))) ; Fold long lines
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,48 +4,119 @@
 ;; Global customisation
 (load "~/.emacs.d/glob_cust/glob_cust.el" nil t t)
 
-;; el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-;; Downloads el-get:
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-;; Where to store recipes
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-;; My own init files pr. mode:
-(setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
+;; ;; el-get
+;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; ;; Downloads el-get:
+;; (unless (require 'el-get nil 'noerror)
+;;   (with-current-buffer
+;;       (url-retrieve-synchronously
+;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+;;     (goto-char (point-max))
+;;     (eval-print-last-sexp)))
+;; ;; Where to store recipes
+;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;; (el-get 'sync)
+;; ;; My own init files pr. mode:
+;; (setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
 
-;; El get packages
-(setq my-packages
-      '(
-        visual-fill-column
-	auctex
-	bash-completion
-	ein
-	el-get
-	magit
-	magit-gitflow
-        magit-popup
-	markdown-mode
-	openwith
-	restclient
-        flyspell
-        ;; python-mode ;; need to run el-get-elpa-build-local-recipes ?
-        ;; elpy
-        ;; python
-        ;; https://smythp.com/emacs/python/2016/04/27/pyenv-elpy.html
-        flycheck ;; need to run el-get-elpa-build-local-recipes ?
-        ;; py-autopep8
-        ;; blacken
-        ;; pyenv-mode f ;; manually installed dependency: f
-        ;; pyenv-mode-auto
-	))
-(el-get 'sync my-packages)
+;; ;; El get packages
+;; (setq my-packages
+;;       '(
+;;         visual-fill-column
+;; 	bash-completion
+;; 	ein
+;; 	el-get
+;; 	markdown-mode
+;; 	openwith
+;; 	restclient
+;;         ;; python-mode ;; need to run el-get-elpa-build-local-recipes ?
+;;         ;; elpy
+;;         ;; python
+;;         ;; https://smythp.com/emacs/python/2016/04/27/pyenv-elpy.html
+;;         flycheck ;; need to run el-get-elpa-build-local-recipes ?
+;;         ;; py-autopep8
+;;         ;; blacken
+;;         ;; pyenv-mode f ;; manually installed dependency: f
+;;         ;; pyenv-mode-auto
+;; 	))
+;; (el-get 'sync my-packages)
 
+;; Trying out package.el and use-package
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(package-initialize)
+;; Download use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+;; Reduce load time
+(eval-when-compile (require 'use-package))
+
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :ensure org-plus-contrib
+  :bind (:map org-mode-map
+              ("C-c l" . org-store-link)
+              ("C-c a" . org-agenda)
+              )
+  :config
+  (progn
+    (setq org-log-done t)
+    (setq org-agenda-files (list "~/notes.org"))
+    (setq org-todo-keywords
+          '((sequence "TODO"  "WAITNG" "|" "DONE" "DELEGATED" "SCRAPPED")))
+    (add-to-list 'org-modules 'org-tempo)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((python . t)))
+    ))
+
+(use-package tex
+  :ensure auctex ; Because package and mode file names are not the same
+  :config (load-file "~/.emacs.d/el-get-init-files/init-auctex.el") ;; TODO: Clean this up
+  )
+
+(use-package magit
+  :ensure t
+  :config
+  (progn
+    (setq magit-last-seen-setup-instructions "1.4.0")
+    (setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
+    (setq magit-diff-refine-hunk (quote all))
+    (add-hook 'magit-mode-hook (lambda () (toggle-truncate-lines -1))) ; Fold long lines
+    ))
+
+(use-package magit-gitflow
+  :ensure t
+  :after magit
+  :config
+  (progn
+    (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+    (setq magit-gitflow-feature-start-arguments (quote ("--fetch")))
+    ))
+
+(use-package flyspell 
+    :after (org python)
+    :config
+    (flyspell-mode))
+
+(use-package visual-fill-column
+  :ensure t
+  :after (auctex)
+  )
+
+;; (use-package flycheck
+;;   :ensure t
+;;   :commands flycheck-mode
+;;   :init (add-hook 'prog-mode-hook 'flycheck-mode)
+;;   :config
+;;   (progn
+;;     (setq-default flycheck-emacs-lisp-initialize-packages t
+;;                   flycheck-highlighting-mode 'lines
+;;                   flycheck-check-syntax-automatically '(save)
+;;                   flycheck-disabled-checkers '(c/c++-clang c/c++-gcc))
+;;     ))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -53,10 +124,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(indent-tabs-mode nil)
- '(initial-major-mode (quote markdown-mode))
  '(initial-scratch-message nil)
- '(safe-local-variable-values (quote ((encoding . utf-8))))
- '(ispell-dictionary "en_GB"))
+ '(ispell-dictionary "en_GB")
+ '(package-selected-packages (quote (use-package org-plus-contrib)))
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
