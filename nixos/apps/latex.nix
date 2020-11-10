b2369b2d293761f0c26b3559b1fa139f7ea7283d
Egon Kidmose <egk@es.aau.dk>
2020-11-10 21:31:58 +0100

    [nixos] latex filter for AUCTeX

--- nixos/apps/latex.nix
+++ nixos/apps/latex.nix
@@ -1,11 +1,23 @@
 # My setup for working with LaTeX
 { config, pkgs, ... }:
+let
+  ignoreauctex = pkgs.writeTextFile {
+    # Strips lines that are AUCTeX in-file settings, for use as a git filter (clean)
+    name = "ignoreauctex";
+    executable = true;
+    destination = "/bin/ignoreauctex";
+    text = ''
+      #!/usr/bin/env bash
+      ${pkgs.gnused}/bin/sed -E '/^%%% (Local Variables:|mode: latex|TeX-master:.*|End:|TeX-command-extra-options:.*)$/d'
+    '';
+  };
+in
 {
   imports =
     [
       ./emacs.nix # I'm sure this can be done more "nixy"
     ];
-  
+
   environment.systemPackages = with pkgs; [
     # LaTeX stuff
     (texlive.combine {inherit (texlive) scheme-minimal
@@ -20,8 +32,10 @@
       collection-publishers # elsarticle.sty
       metafont # mf command line util for fonts (latex package ifsym)
       tex4ht # htlatex to build html
+      biblatex
                       ;})
     evince
     ghostscript # for ps2pdf
+    ignoreauctex
   ];
 }
